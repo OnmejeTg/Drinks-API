bb4c126febf38e40f09570af29f34f3d10ec8612
tgod <tonmeje@gmail.com>
2022-11-21 00:03:29 +0100

    added Create EndPoint

--- main/views.py
+++ main/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Drink
+from .serializers import DrinkSerialiser
+from django.http import JsonResponse
+from rest_framework.response import Response
 
 # Create your views here.
+def drink_list(request):
+    drinks = Drink.objects.all()
+    serializer = DrinkSerialiser(drinks, many=True)
+
+    return JsonResponse(serializer.data, safe=False)
